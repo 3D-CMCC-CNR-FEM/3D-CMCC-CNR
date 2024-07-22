6713a114e28c00c9a7b96850b08d9fa61da54a41
ddalmo <daniela.dalmonech@gmail.com>
2024-07-22 16:03:36 +0200

    update arguments in input

--- software/3D-CMCC-Forest-Model/headers/modifiers.h
+++ software/3D-CMCC-Forest-Model/headers/modifiers.h
@@ -4,6 +4,7 @@
 
 #include "matrix.h"
 
-void modifiers(cell_t *const c, const int layer, const int height, const int dbh, const int age, const int species, const meteo_daily_t *const meteo_daily,	const meteo_annual_t *const meteo_annual);
+void modifiers(cell_t *const c, const int layer, const int height, const int dbh, const int age, const int species, const meteo_daily_t *const meteo_daily,	const meteo_annual_t *const meteo_annual ,  const int month , const int day);
+//5p606 added month and doy
 
 #endif /* MODIFIERS_H_ */
