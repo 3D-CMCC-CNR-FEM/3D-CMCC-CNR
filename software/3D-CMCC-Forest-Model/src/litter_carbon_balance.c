efe0958fb04774fb5a031e94ba0f73e34b63d8ae
ddalmo <daniela.dalmonech@gmail.com>
2024-09-05 19:16:52 +0200

    added C flux from reserve to litter

--- software/3D-CMCC-Forest-Model/src/litter_carbon_balance.c
+++ software/3D-CMCC-Forest-Model/src/litter_carbon_balance.c
@@ -34,11 +34,12 @@ void litter_carbon_balance ( cell_t *const c, const int year )
 	/*********************************************************************************************************/
 
 	/* update carbon fluxes balance of labile litter pool */
-	c->daily_to_litr1C          = ( c->daily_leaf_to_litr1C + c->daily_froot_to_litr1C )
+	c->daily_to_litr1C          = ( c->daily_leaf_to_litr1C + c->daily_froot_to_litr1C +c->daily_reserve_to_litr1C)
 					- ( c-> daily_litr1_het_resp + c->daily_litr1C_to_soil1C );
-
+					
 	/* update carbon mass of labile litter pool */
 	c->litr1C                  += c->daily_to_litr1C;
+
 	/*********************************************************************************************************/
 
 	/* update carbon fluxes balance of cellulose litter pool */
@@ -63,11 +64,13 @@ void litter_carbon_balance ( cell_t *const c, const int year )
 
 	/* update carbon mass of lignin litter pool */
 	c->litr4C                  += c->daily_to_litr4C;
+
 	/*********************************************************************************************************/
 
 	/* total fluxes  */
 	c->daily_leaf_to_litrC      = c->daily_leaf_to_litr1C  + c->daily_leaf_to_litr2C     + c->daily_leaf_to_litr3C     + c->daily_leaf_to_litr4C;
 	c->daily_froot_to_litrC     = c->daily_froot_to_litr1C + c->daily_froot_to_litr2C    + c->daily_froot_to_litr3C    + c->daily_froot_to_litr4C;
+	c->daily_reserve_to_litrC      = c->daily_reserve_to_litr1C ;
 	c->daily_cwd_to_litrC       =                            c->daily_cwd_to_litr2C      + c->daily_cwd_to_litr3C      + c->daily_cwd_to_litr4C;
 	c->daily_to_litrC           = c->daily_to_litr1C       + c->daily_to_litr2C          + c->daily_to_litr3C          + c->daily_to_litr4C;
 
@@ -76,12 +79,12 @@ void litter_carbon_balance ( cell_t *const c, const int year )
 	c->litrC                    = c->litr1C + c->litr2C      + c->litr3C      + c->litr4C;
 
 	/* move from litter to soil pools (this need to be done here to close litter balance) */
+
 	c->daily_to_soilC           = c->daily_litr1C_to_soil1C + c->daily_litr2C_to_soil2C + c->daily_litr4C_to_soil3C;
 	c->daily_to_soil1C          = c->daily_litr1C_to_soil1C;
 	c->daily_to_soil2C          = c->daily_litr2C_to_soil2C;
 	c->daily_to_soil3C          = c->daily_litr4C_to_soil3C;
 	c->daily_to_soil4C          = 0.;
-
 
 }
 
