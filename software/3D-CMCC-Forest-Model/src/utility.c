54a5a7ab4dd0316902ebc8f91a49464fd462da20
ddalmo <daniela.dalmonech@gmail.com>
2024-11-04 11:22:32 +0100

    removed empty line

--- software/3D-CMCC-Forest-Model/src/utility.c
+++ software/3D-CMCC-Forest-Model/src/utility.c
@@ -251,7 +251,7 @@ void reset_annual_cell_variables(cell_t *const c)
 	c->agb =                            0.;
 	c->bgb =                            0.;
 	c->volume =                         0.;
-	c->yearly_C_to_wood =               0.;
+	c->yearly_C_to_wood =               0.;	
 	//c->dead_tree =                    0;
 	c->daily_dead_tree  =               0;
 	c->annual_soil_evapo =              0.;
