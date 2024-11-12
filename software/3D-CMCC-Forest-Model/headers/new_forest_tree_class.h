f9e68340de6dafd5a507c4c691899df43f9401ea
ddalmo <daniela.dalmonech@gmail.com>
2024-11-12 08:57:32 +0100

    removed rsi among input variables

--- software/3D-CMCC-Forest-Model/headers/new_forest_tree_class.h
+++ software/3D-CMCC-Forest-Model/headers/new_forest_tree_class.h
@@ -10,8 +10,8 @@
 
 #include "matrix.h"
 
-int add_tree_class_for_replanting (cell_t *const c, const int day, const int month, const int year, const int rsi);
-
+//int add_tree_class_for_replanting (cell_t *const c, const int day, const int month, const int year, const int rsi);
+int add_tree_class_for_replanting (cell_t *const c, const int day, const int month, const int year);
 int add_tree_class_for_regeneration ( cell_t *const c );
 
 //static int fill_cell(cell_t *const c);
