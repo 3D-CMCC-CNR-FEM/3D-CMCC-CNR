0fbd68bbd32e10b743c1af6a8111f3d6e64e053d
ddalmo <daniela.dalmonech@gmail.com>
2024-12-19 14:26:49 +0100

    added new function to compute seeds from NPP

--- software/3D-CMCC-Forest-Model/headers/C-fruit-partitioning.h
+++ software/3D-CMCC-Forest-Model/headers/C-fruit-partitioning.h
@@ -14,3 +14,22 @@ void Daily_C_Evergreen_Partitioning_Allocation (species_t *const s, cell_t *cons
 		const int years, const int DaysInMonth, const int height, const int age, const int species);
 
 #endif /* SRC_C_FRUIT_PARTITIONING_ALLOCATION_H_ */
+
+/*
+ * C-fruit-partitioning.h
+ *
+ * Created on: 21 sep 2022
+ * Author: Vincenzo Saponaro
+ */
+
+#ifndef C_FRUIT_PARTITIONING_H_
+#define C_FRUIT_PARTITIONING_H_
+
+#include "matrix.h"
+
+//int Fruit_to_seeds_function (cell_t *const c, const int layer, const age_t *const a, species_t *const s, const int year);
+void Fruit_to_seeds_function_npp (cell_t *const c, const age_t *const a, species_t *const s, const int day, const int month, const int year);
+//void Fruit_to_seeds_function_structure (cell_t *const c, const int height, const int dbh, const int age, const int species, const int day, const int month, const int year);
+//void Fruit_to_seeds_function_age(cell_t *const c, const int height, const int dbh, const int age, const int species, const int day, const int month, const int year);
+
+#endif /* C_FRUIT_PARTITIONING_H_ */
