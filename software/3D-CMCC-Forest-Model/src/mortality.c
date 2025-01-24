eeac9718baa42a05008621125581ba85ac2af34b
ddalmo <daniela.dalmonech@gmail.com>
2025-01-24 10:48:53 +0100

    updated

--- software/3D-CMCC-Forest-Model/src/mortality.c
+++ software/3D-CMCC-Forest-Model/src/mortality.c
@@ -94,6 +94,11 @@ void self_thinning_mortality_new(cell_t *const c, const int layer, const int yea
 		{
 			d = &c->heights[height].dbhs[dbh];
 
+			// Skip selt-thinning tree removal is large trees
+
+			if (d >= self_thinning_treshold)  goto next_dbh;
+
+
 			for ( age = 0; age < d->ages_count ; ++age )
 			{
 				a = &c->heights[height].dbhs[dbh].ages[age];
