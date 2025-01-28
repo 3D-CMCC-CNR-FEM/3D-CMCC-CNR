cabb5b5bc0a10569fe1ad7a29bf6eea1f18f6159
ddalmo <daniela.dalmonech@gmail.com>
2025-01-28 11:14:07 +0100

    change name from EOY_print_output_class_level_management to EOY_print_output_class_level_mortality

--- software/3D-CMCC-Forest-Model/headers/print_output.h
+++ software/3D-CMCC-Forest-Model/headers/print_output.h
@@ -15,7 +15,7 @@ void EOM_cell_msg(void);
 void EOY_print_output_cell_level(cell_t *const, const int, const int);
 void EOY_cell_msg(void);
 
-void EOY_print_output_class_level_management(cell_t *const c, const int height, const int dbh , const int age , const int species, const int year );
+void EOY_print_output_class_level_mortality(cell_t *const c, const int height, const int dbh , const int age , const int species, const int year );
 //void EOY_cell_msg(void);
 
 
